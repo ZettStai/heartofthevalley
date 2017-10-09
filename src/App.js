238f7e43e0c338014d7e4695d18a9f10f04ee5fd
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-10-09 16:54:22 -0700

    add imports

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MuralList from './MuralList';
+import MuralForm from './MuralForm';
+import DATA from './data';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <h2>Murals:</h2>
+      <MuralList data={ DATA }/>
+      <MuralForm />
+    )
   }
 }
 
